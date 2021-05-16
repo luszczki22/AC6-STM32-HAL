56b9a65e823333b7bfc30afaff648ccd7d5bfe06
Marcin-komputer\Marcin <dzzirt22@gmail.com>
2021-05-17 00:14:02 +0200

    LED line

--- projekt1/HelloSTM32/src/main.c
+++ projekt1/HelloSTM32/src/main.c
@@ -32,20 +32,37 @@ int main(void)
 	//__HAL_RCC_GPIOD_CLK_ENABLE();
 
 	GPIO_InitTypeDef gpio; // obiekt gpio bêd¹cy konfiguracj¹ portów GPIO
-	gpio.Pin= GPIO_PIN_5;	// konfigurujemy pin 5
+	gpio.Pin = GPIO_PIN_5;	// konfigurujemy pin 5
 	gpio.Mode = GPIO_MODE_OUTPUT_PP;	// jako wyjœcie
 	gpio.Pull = GPIO_NOPULL;	// rezystory podci¹gaj¹ce s¹ wy³¹czone
 	gpio.Speed = GPIO_SPEED_FREQ_LOW;	// wystarcz¹ nieskie czêstotliwoœci prze³¹czania
 	HAL_GPIO_Init(GPIOA, &gpio);	// inicjalizacja modu³u GPIOA
+
+	gpio.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|
+			 GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;	// jako wyjœcie
+	gpio.Pull = GPIO_NOPULL;	// rezystory podci¹gaj¹ce s¹ wy³¹czone
+	gpio.Speed = GPIO_SPEED_FREQ_LOW;	// wystarcz¹ nieskie czêstotliwoœci prze³¹czania
+	HAL_GPIO_Init(GPIOC, &gpio);	// inicjalizacja modu³u GPIOA
 
 	gpio.Pin = GPIO_PIN_13; //konfigurujemy pin 13
 	gpio.Mode = GPIO_MODE_INPUT; ///jako wejscie
 	gpio.Pull = GPIO_PULLUP;	//wlaczamy rezystro podciagajacy
 	HAL_GPIO_Init(GPIOC, &gpio);	//port GPIOC
 
+	uint32_t led = 0;
 
 	while (1)
 	{
+		HAL_GPIO_WritePin(GPIOC, 1 << led, GPIO_PIN_SET);
+		HAL_Delay(150);
+		HAL_GPIO_WritePin(GPIOC, 1 << led, GPIO_PIN_RESET);
+		if(++led >= 10) // przejdz do nastepnej
+		{
+			led = 0;
+		}
+
+
 		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, GPIO_PIN_SET);
 		HAL_Delay(100);
 		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, GPIO_PIN_RESET);
