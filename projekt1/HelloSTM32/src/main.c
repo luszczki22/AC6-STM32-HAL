e810cad904b4e5b6fa605475a7bf5778232c90b4
Marcin-komputer\Marcin <dzzirt22@gmail.com>
2021-05-26 01:05:13 +0200

    PWM TIM1 4 channels

--- projekt1/HelloSTM32/src/main.c
+++ projekt1/HelloSTM32/src/main.c
@@ -58,6 +58,25 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
 	else
 		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_SET);
+}
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	switch(htim->Channel){
+	case HAL_TIM_ACTIVE_CHANNEL_1:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+		break;
+	case HAL_TIM_ACTIVE_CHANNEL_2:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
+		break;
+	case HAL_TIM_ACTIVE_CHANNEL_3:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+		break;
+	case HAL_TIM_ACTIVE_CHANNEL_4:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_RESET);
+		break;
+	}
 }
 
 int main(void)
@@ -143,7 +162,7 @@ int main(void)
 	HAL_ADC_Start(&adc);
 
 	tim2.Instance = TIM2;
-	tim2.Init.Period = 1000 - 1;
+	tim2.Init.Period = 2000 - 1;
 	tim2.Init.Prescaler = 8000 - 1;
 	tim2.Init.ClockDivision = 0;
 	tim2.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -156,6 +175,13 @@ int main(void)
 	HAL_NVIC_EnableIRQ(TIM2_IRQn);
 
 	HAL_TIM_Base_Start_IT(&tim2);
+
+	__HAL_TIM_SET_COMPARE(&tim2, TIM_CHANNEL_1, 100);
+	__HAL_TIM_SET_COMPARE(&tim2, TIM_CHANNEL_2, 200);
+	__HAL_TIM_SET_COMPARE(&tim2, TIM_CHANNEL_3, 500);
+	__HAL_TIM_SET_COMPARE(&tim2, TIM_CHANNEL_4, 900);
+
+	__HAL_TIM_ENABLE_IT(&tim2, TIM_IT_CC1|TIM_IT_CC2|TIM_IT_CC3|TIM_IT_CC4);
 
 	uint32_t led = 0;
 
